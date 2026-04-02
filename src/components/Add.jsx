8eab4be9c21dab7935179a3db8404e57232047bb
premanshu <premjha@lentra.ai>
2026-04-02 09:27:36 +0530

    did addition and deletion of ingredients

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,10 +1,33 @@
+import { useState } from 'react'
 import './Add.css'
+import Ingredients from './Ingredients';
 
 export default function Add(){
+    let [list,setList] = useState([]);
+    let [ingredient,setIngredient] = useState('');
+
+
+    const handleAddition = (value) => {
+         value = value.trim();
+         if(value.length > 0){
+            setList(prev=>[...prev,{id:Date.now(),value}]);
+            setIngredient('');
+         }
+    }
+
+    const handleDelete = (id)=>{
+         setList(prev => prev.filter(item=>item.id!=id));
+    }
+
     return (
+        <div className="addCon">
         <div className="add">
-          <input type="text" placeholder="e.g. oregano" class="input" />
-          <button className="btn btn-primary btn-base">+ Add Ingredient</button>
+          <input type="text" placeholder="e.g. oregano" className="input" value={ingredient} onChange={event=>setIngredient(event.target.value)}/>
+          <button className="btn btn-primary btn-base" onClick={()=>handleAddition(ingredient)}>+ Add Ingredient</button>
+        </div>
+        <div>
+            <Ingredients list={list} handleDelete={handleDelete}/>
+        </div>
         </div>
     )
 }
---
+++ src/components/Ingredients.jsx
@@ -0,0 +1,24 @@
+import DeleteIcon from "./svgs"
+import './Ingredients.css'
+
+
+export default function Ingredients({list,handleDelete}){
+
+    return <div className="listCon">
+    <span className='title'>{list.length > 0?'Ingredients on hand:':''}</span>
+    <ul>
+    {
+    list.map((item)=>{
+        return <li key={item.id} className="listCon">
+            <div className="listItem">
+            <span className="itemName">{item.value}</span>
+            <button className="delete-btn" onClick={()=>handleDelete(item.id)}>
+                <DeleteIcon />
+               </button>
+            </div>
+        </li>
+    })
+}
+</ul>
+</div>
+}
